9f705cb5a9cef4afb15e0385052e78be2269a7f6
parastoo marzi <parastoo@gardoon.io>
2024-02-07 16:50:55 +0330

    new4

--- lib/session.ts
+++ lib/session.ts
@@ -1,7 +1,10 @@
 import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
+//import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github"
+
+
 import jsonwebtoken from 'jsonwebtoken'
 import { JWT } from "next-auth/jwt";
 
@@ -10,10 +13,17 @@ import { SessionInterface, UserProfile } from "@/common.types";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    //GoogleProvider({
+      //clientId: process.env.GOOGLE_CLIENT_ID!,
+      //clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    //}),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
+
+
+
   ],
   jwt: {
     encode: ({ secret, token }) => {
